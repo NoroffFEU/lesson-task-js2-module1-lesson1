ee78c0a5e364cb88770f1cabfba338154de120aa
Martin Kruger <martin@martinkruger.com>
2022-02-01 16:29:41 +0200

    Updated for rewrite

---
+++ question-1.js
@@ -0,0 +1,12 @@
+// Question 1:
+
+const game = {
+  id: 0,
+  title: "Street Rod 2",
+  genre: "Racing",
+  rating: 5,
+  multiplayer: false,
+  year: 1992,
+  publisher: ["California Dreams"],
+  developer: ["Logical Design Works, Inc.", "P.Z.Karen Co. Development Group"],
+};
---
+++ question-4.js
@@ -0,0 +1,3 @@
+// Question 4:
+
+const temperatures = [101, 95, 102, 112, 98, 89, 109, 100];
