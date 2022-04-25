6a0c56851330ec2f9132e66f8c84a6f9a17a1e76
Martin Kruger <martin@martinkruger.com>
2022-04-25 13:30:13 +0200

    Revert "Fixed question 2 and 3"
    This reverts commit 386d8577ea7a21cba69fca7a37c474d18a71e395.

--- question-2.js
+++ question-2.js
@@ -1,12 +1,13 @@
 // Question 2:
+
 const user = {
-  firstName: 'Ola',
-  lastName: 'Nordmann',
-  currentModule: 'JavaScript 2',
+  firstName: "Ola",
+  lastName: "Ola",
+  module: "JavaScript 2",
 };
 
 const firstName = user.firstName;
 const lastName = user.lastName;
-const currentModule = user.currentModule;
+const isAdmin = user.isAdmin;
 
-const greeting = `My name is ${user.firstName} ${user.lastName} and I am on ${user.currentModule}.`;
+const greeting = `My name is ${user.firstName} ${user.lastName} and I am on ${user.module}.`;
